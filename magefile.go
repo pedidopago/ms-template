fff065143358c7ec00870b75028d799b7fb55283
Gabriel Ochsenhofer <gabs@gabs.tv>
2020-12-30 09:26:43 -0300

    regexp remove install script after setup

--- magefile.go
+++ magefile.go
@@ -12,6 +12,7 @@ import (
 	"net/url"
 	"os"
 	"path"
+	"regexp"
 	"strings"
 	"time"
 
@@ -29,20 +30,6 @@ const (
 	dbfiles = "file://%GIT_ROOT%/internal/database/migrations"
 	dbcs    = "mysql://testuser:123456789@tcp(localhost)/ms_xyz?parseTime=true"
 )
-
-func xs(v string) string {
-	if strings.Contains(v, "%GIT_ROOT%") {
-		rootp, _ := xcmd.CombinedString("git", "rev-parse", "--show-toplevel")
-		v = strings.Replace(v, "%GIT_ROOT%", rootp, -1)
-	}
-	return v
-}
-
-func loadEnvs() {
-	if _, e := os.Stat("dbm.env"); e == nil {
-		godotenv.Overload("dbm.env")
-	}
-}
 
 func Migrate(version string) error {
 	loadEnvs()
@@ -97,6 +84,88 @@ func Newmigration(name string) error {
 }
 
 func Setup() error {
+	// # remove >>
+	if err := setupInstall(); err != nil {
+		return err
+	}
+	// # remove <<
+	if _, e := os.Stat("dbm.env"); e != nil {
+		nd, _ := ioutil.ReadFile(".name")
+		name := strings.TrimSpace(string(nd))
+		print("mariadb user (testuser): ")
+
+		rdr := bufio.NewReader(os.Stdin)
+		str, err := rdr.ReadString('\n')
+		if err != nil {
+			return err
+		}
+		str = strings.TrimSpace(str)
+		if str == "" {
+			str = "testuser"
+		}
+		dbuser := "testuser"
+
+		println("")
+		print("mariadb password (123456789): ")
+
+		str, err = rdr.ReadString('\n')
+		if err != nil {
+			return err
+		}
+		str = strings.TrimSpace(str)
+		if str == "" {
+			str = "123456789"
+		}
+		dbpass := str
+
+		println("please run this:")
+		println("CREATE DATABASE IF NOT EXISTS `ms_" + name + "`;")
+
+		ndbcs := dbuser + ":" + dbpass + "@tcp(localhost)/ms_" + name + "?parseTime=true"
+
+		if err := replaceStringInFile("cmd/"+name+"/main.go", "testuser:123456789@tcp(localhost)/ms_xyz?parseTime=true", ndbcs); err != nil {
+			return err
+		}
+
+		if err := ioutil.WriteFile("dbm.env", []byte("DB_FILES="+dbfiles+"\nDB_CS="+ndbcs+"\nOPEN_CMD=code\nDOCKER_REGISTRY=registry.docker.pedidopago.com.br/ms/"+name), 0644); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func Devbuild() error {
+	//
+	nd, _ := ioutil.ReadFile(".name")
+	name := strings.TrimSpace(string(nd))
+	//
+	_ = sh.Run("mkdir", "-p", "tmp")
+	_, err := sh.Exec(map[string]string{
+		"GOOS":   "linux",
+		"GOARCH": "amd64",
+	}, os.Stdout, os.Stderr, "go", "build", "-o", "tmp/service_linux_x64", "cmd/"+name+"/main.go")
+	return err
+}
+
+func Devdocker() error {
+	loadEnvs()
+	//
+	nd, _ := ioutil.ReadFile(".name")
+	name := strings.TrimSpace(string(nd))
+	//
+	if err := Devbuild(); err != nil {
+		return err
+	}
+
+	version := defaults.String(os.Getenv("VERSION"), "latest")
+	registry := defaults.String(os.Getenv("DOCKER_REGISTRY"), "registry.docker.pedidopago.com.br/ms/"+name)
+
+	// docker build --build-arg VERSION=${VERSION} -t ${REGISTRY}:${VERSION} .
+	return sh.Run("docker", "build", "--build-arg", "VERSION="+version, "-t", registry+":"+version, "-f", "dev.Dockerfile", ".")
+}
+
+// # remove >>
+func setupInstall() error {
 	if _, e := os.Stat(".name"); e != nil {
 		print("service name: ")
 		rdr := bufio.NewReader(os.Stdin)
@@ -174,81 +243,17 @@ func Setup() error {
 		if err := ioutil.WriteFile(".name", []byte(name), 0644); err != nil {
 			return err
 		}
-	}
-	if _, e := os.Stat("dbm.env"); e != nil {
-		nd, _ := ioutil.ReadFile(".name")
-		name := strings.TrimSpace(string(nd))
-		print("mariadb user (testuser): ")
 
-		rdr := bufio.NewReader(os.Stdin)
-		str, err := rdr.ReadString('\n')
-		if err != nil {
-			return err
-		}
-		str = strings.TrimSpace(str)
-		if str == "" {
-			str = "testuser"
-		}
-		dbuser := "testuser"
-
-		println("")
-		print("mariadb password (123456789): ")
-
-		str, err = rdr.ReadString('\n')
-		if err != nil {
-			return err
-		}
-		str = strings.TrimSpace(str)
-		if str == "" {
-			str = "123456789"
-		}
-		dbpass := str
-
-		println("please run this:")
-		println("CREATE DATABASE IF NOT EXISTS `ms_" + name + "`;")
-
-		ndbcs := dbuser + ":" + dbpass + "@tcp(localhost)/ms_" + name + "?parseTime=true"
-
-		if err := replaceStringInFile("cmd/"+name+"/main.go", "testuser:123456789@tcp(localhost)/ms_xyz?parseTime=true", ndbcs); err != nil {
-			return err
-		}
-
-		if err := ioutil.WriteFile("dbm.env", []byte("DB_FILES="+dbfiles+"\nDB_CS="+ndbcs+"\nOPEN_CMD=code\nDOCKER_REGISTRY=registry.docker.pedidopago.com.br/ms/"+name), 0644); err != nil {
+		// remove self install
+		if err := regexpRemoveStringInFile("magefile.go", regexp.MustCompile(`(?s)// # remove >>.*// # remove <<`)); err != nil {
 			return err
 		}
 	}
+
 	return nil
 }
 
-func Devbuild() error {
-	//
-	nd, _ := ioutil.ReadFile(".name")
-	name := strings.TrimSpace(string(nd))
-	//
-	_ = sh.Run("mkdir", "-p", "tmp")
-	_, err := sh.Exec(map[string]string{
-		"GOOS":   "linux",
-		"GOARCH": "amd64",
-	}, os.Stdout, os.Stderr, "go", "build", "-o", "tmp/service_linux_x64", "cmd/"+name+"/main.go")
-	return err
-}
-
-func Devdocker() error {
-	loadEnvs()
-	//
-	nd, _ := ioutil.ReadFile(".name")
-	name := strings.TrimSpace(string(nd))
-	//
-	if err := Devbuild(); err != nil {
-		return err
-	}
-
-	version := defaults.String(os.Getenv("VERSION"), "latest")
-	registry := defaults.String(os.Getenv("DOCKER_REGISTRY"), "registry.docker.pedidopago.com.br/ms/"+name)
-
-	// docker build --build-arg VERSION=${VERSION} -t ${REGISTRY}:${VERSION} .
-	return sh.Run("docker", "build", "--build-arg", "VERSION="+version, "-t", registry+":"+version, "-f", "dev.Dockerfile", ".")
-}
+// # remove <<
 
 func replaceStringInFile(fname, oldv, newv string) error {
 	fi, err := os.Stat(fname)
@@ -261,4 +266,31 @@ func replaceStringInFile(fname, oldv, newv string) error {
 	}
 	ds := strings.Replace(string(d), oldv, newv, -1)
 	return ioutil.WriteFile(fname, []byte(ds), fi.Mode().Perm())
+}
+
+func regexpRemoveStringInFile(fname string, exp *regexp.Regexp) error {
+	fi, err := os.Stat(fname)
+	if err != nil {
+		return err
+	}
+	d, err := ioutil.ReadFile(fname)
+	if err != nil {
+		return err
+	}
+	d = exp.ReplaceAll(d, []byte(""))
+	return ioutil.WriteFile(fname, d, fi.Mode().Perm())
+}
+
+func xs(v string) string {
+	if strings.Contains(v, "%GIT_ROOT%") {
+		rootp, _ := xcmd.CombinedString("git", "rev-parse", "--show-toplevel")
+		v = strings.Replace(v, "%GIT_ROOT%", rootp, -1)
+	}
+	return v
+}
+
+func loadEnvs() {
+	if _, e := os.Stat("dbm.env"); e == nil {
+		godotenv.Overload("dbm.env")
+	}
 }
