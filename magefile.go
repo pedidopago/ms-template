9702ce328c22cb7dafcf8f99ed90adade4ad089c
Gabriel Ochsenhofer <gabs@gabs.tv>
2021-01-12 20:06:55 -0300

    lazy regexp & remove slugify for module name

--- magefile.go
+++ magefile.go
@@ -263,7 +263,7 @@ func setupInstall() error {
 			return err
 		}
 		str = strings.TrimSpace(str)
-		module := slugify.Slugify(str)
+		module := str
 		if module == "" {
 			return errors.New("invalid module name")
 		}
@@ -364,7 +364,7 @@ func setupInstall() error {
 		}
 
 		// remove self install
-		if err := regexpRemoveStringInFile("magefile.go", regexp.MustCompile(`(?s)// # remove >>.*// # remove <<`)); err != nil {
+		if err := regexpRemoveStringInFile("magefile.go", regexp.MustCompile(`(?s)// # remove >>.*?// # remove <<`)); err != nil {
 			return err
 		}
 	}
